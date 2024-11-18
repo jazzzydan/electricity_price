73421c88e37642f07428e4f83694535618037cf7
Daniil Starov & Mikk Sarv <jazzzydan@hotmail.com & mikksrv@gmail.com>
2024-11-18 16:31:47 +0200

    test switching from summer time to winter time (25 bars on the chart for that night)

--- src/components/barchart/BarChart.test.ts
+++ src/components/barchart/BarChart.test.ts
@@ -1,1 +1,31 @@
-//TODO: test time switch from summer to winter time and vice versa
+import {describe, expect, it} from "vitest";
+import type {ApiResponse, PricePair} from "../../utilities/apiClient";
+import {render} from "@testing-library/svelte";
+import BarChart from "./BarChart.svelte";
+import {getPriceDataForCountry} from "../../utilities/dataMapper";
+
+describe('BarChart', () => {
+    let testResponseData = {
+        success: true,
+        data: {
+            'ee': []
+        }
+    } as ApiResponse
+
+    for (let i = 0; i < 25; i++) {
+        testResponseData.data.ee.push({
+            timestamp: i + 1730890800,
+            price: i + 74.8300
+        }) as PricePair
+    }
+    console.log(testResponseData)
+
+    it('should render the chart with 25 bars on the night of time switch form summer to winter time', async () => {
+        const priceDataForCountry = getPriceDataForCountry(testResponseData, 'ee')
+        const isToday = false
+
+        const {container} = render(BarChart, {priceDataForCountry, isToday})
+
+        expect(container.querySelectorAll('.bar')).to.have.length(25)
+    })
+})
